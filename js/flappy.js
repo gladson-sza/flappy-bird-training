31f33e3d311cce16bdb7764e39aa79e9457d6b64
gladson-sza <gladsonsouza@live.com>
2021-01-05 16:16:56 -0400

    Animating bird

--- js/flappy.js
+++ js/flappy.js
@@ -54,8 +54,6 @@ function Barriers(height, width, opening, space, notifyScore) {
       this.pairs.forEach(pair => {
          pair.setX(pair.getX() - displacement)
 
-         console.log('Pair: ', pair.getX())
-
          if (pair.getX() < -pair.getWidth()) {
             pair.setX(pair.getX() + space * this.pairs.length)
             pair.sortOpening()
@@ -71,12 +69,49 @@ function Barriers(height, width, opening, space, notifyScore) {
    }
 }
 
-const barriers = new Barriers(700, 1200, 200, 400)
+function Bird(gameHeight) {
+   let isFlying = false
+
+   this.element = newElement('img', 'bird')
+   this.element.src = 'imgs/flappy-bird.png'
+
+   this.getY = () => parseInt(this.element.style.bottom.split('px')[0])
+   this.setY = y => this.element.style.bottom = `${y}px`
+   
+   window.onkeydown = e => isFlying = true
+   window.onkeyup = e => isFlying = false
+
+   this.animate = () => {
+      const toUp = 8
+      const toDown = -5
+
+      const newY = this.getY() + (isFlying ? toUp : toDown)
+      const maxHeight = gameHeight - this.element.clientHeight
+
+      if (newY < 0) {
+         this.setY(0)
+      } else if (newY >= maxHeight) {
+         this.setY(maxHeight)
+      } else {
+         this.setY(newY)
+      }
+   }
+
+   this.setY(gameHeight / 2)
+}
+
+const defaultHeight = 500
+
+const barriers = new Barriers(defaultHeight, 1200, 200, 400)
+const bird = new Bird(defaultHeight)
+
 const gameArea = document.querySelector('[fb-flappy]')
+gameArea.appendChild(bird.element)
 barriers.pairs.forEach(pair => {
    gameArea.appendChild(pair.element)
 })
 
 setInterval(() => {
    barriers.animate()
+   bird.animate()
 }, 20)
